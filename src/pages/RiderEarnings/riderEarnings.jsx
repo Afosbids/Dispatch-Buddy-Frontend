e04e8a04e91ba4609f4b392c4e7a686d290e57bf
Decagon <decagon@Decagons-MacBook-Air-7.local>
2022-06-29 14:53:59 +0100

    added file

---
+++ src/pages/RiderEarnings/riderEarnings.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+const RiderEarnings = () => {
+    return (
+        <>
+        <nav>
+            <div className="left">
+                
+            </div>
+        </nav>
+        <header className="magenta-div"></header>
+        <div>
+            <div className="container1">
+                <p>MY EARNINGS</p>
+                <br/>
+                <h1>N236,000</h1>
+                <div>
+                    <img src="./pages/RiderEarnings/images/small-car.png" alt=""/>
+                    <p>112 Rides</p>
+                </div>
+                <div>
+                    <img src="./pages/RiderEarnings/images/small-car.png" alt=""/>
+                    <p>890hrs . 20mins</p>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+export default RiderEarnings;
