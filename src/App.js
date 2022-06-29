5141ebe19aeb8082d4c759a91ac8086c37f0200d
Decagon <decagon@Decagons-MacBook-Air-7.local>
2022-06-29 12:37:17 +0100

    rider earnings refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import RiderSignup from "./pages/RiderSignup/RiderSignup";
+import RiderEarnings from "./pages/RiderEarnings/riderEarnings";
 import UserSignup from './pages/userSignup/UserSignup'
 import LandingPage from "./pages/LandingPage/LandingPage";
 import UserSignin from "./pages/UserSignin/UserSignin";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from "./components/Navbar/Navbar";
+// import Navbar from "./components/Navbar/Navbar";
 
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import PasswordResetVerification from "./pages/PasswordResetVerification/PasswordResetVerification";
 import ReEnterPassword from "./pages/reEnterPassword/ReEnterPassword";
 import VerifyEmail from "./pages/verifyEmail/VerifyEmail";
-import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
@@ -18,7 +19,7 @@ function App() {
     <>
     
     <BrowserRouter>
-       <Navbar />
+       {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/rider-signup" element={<RiderSignup />} />
@@ -28,10 +29,11 @@ function App() {
         <Route path="/password-link" element={<PasswordResetVerification />} />
         <Route path="/user/verify" element={<VerifyEmail />} />
         <Route path="/user/reset-password" element={<ReEnterPassword />} />
+        <Route path="/rider-earnings" element={<RiderEarnings />} />
       </Routes>
      
     </BrowserRouter>
-    <ToastContainer />
+    {/* <ToastContainer /> */}
     </>
   );
 }
--- src/pages/RiderEarnings/riderEarnings.jsx
+++ src/pages/RiderEarnings/riderEarnings.jsx
@@ -1,16 +1,75 @@
-import React from 'react';
-import logo from './images/logo.png';
+import "./riderEarnings.css";
+import smallcar from "./images/smallcar.svg";
+import time from "./images/Time.svg";
+
 
 const RiderEarnings = () => {
     return (
         <>
         <nav>
-            <div className="left">
-                <img src={logo} alt="" />
-                <p>Dispatch<br/>
-                    Buddy</p>
-            </div>
+            
         </nav>
+        <div className="magenta-div"></div>
+        <br/>
+        <br/>
+        <div className="wrapped-div">
+            <div className="earnings-group">
+                <div className="earnings-group-inner">
+                    <p><strong>Yesterday</strong>, 3:30PM</p>
+                    <div>
+                    <input type="text" placeholder="Delivered" className="earnings-input"/>
+                    <p>N3,500.00</p>
+                    </div>   
+                </div>
+            </div>
+            <br/>
+            <div className="earnings-group">
+                <div className="earnings-group-inner">
+                    <p><strong>Yesterday</strong>, 3:30PM</p>
+                    <div>
+                    <input type="text" placeholder="Delivered" className="earnings-input"/>
+                    <p>N3,500.00</p>
+                    </div>   
+                </div>
+            </div>
+            <br/>
+            <div className="earnings-group">
+                <div className="earnings-group-inner">
+                    <p><strong>Yesterday</strong>, 3:30PM</p>
+                    <div>
+                    <input type="text" placeholder="Delivered" className="earnings-input"/>
+                    <p>N3,500.00</p>
+                    </div>   
+                </div>
+            </div>
+            <div className="earnings-group">
+                <div className="earnings-group-inner">
+                    <p><strong>Today</strong>, 10:15AM</p>
+                    <div>
+                    <input type="text" placeholder="Delivered" className="earnings-input"/>
+                    <p>N2,200.00</p>
+                    </div>   
+                </div>
+            </div>
+            <br/>
+            <div className="container1">
+                <div className="column-text">
+                    <p>MY EARNINGS</p>
+                    <h1>N236,000</h1>
+                </div>
+                
+                <div className="row-text">
+                    <div className="ride-div">
+                        <img src={smallcar} alt=""/>
+                        <p>112 Rides</p>
+                    </div>
+                    <div className="time-div">
+                        <img src={time} alt=""/>
+                        <p>890hrs . 20mins</p>
+                    </div>
+                </div> 
+            </div>
+        </div>
         </>
     );
 }
