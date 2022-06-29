be5df2e9f64fd0d80fa7f9c0e9bf5137e0a31c46
Decagon <decagon@Decagons-MacBook-Air-7.local>
2022-06-29 02:05:15 +0100

    refactor on user signup

---
+++ src/pages/RiderEarnings/riderEarnings.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './images/logo.png';
+
+const RiderEarnings = () => {
+    return (
+        <>
+        <nav>
+            <div className="left">
+                <img src={logo} alt="" />
+                <p>Dispatch<br/>
+                    Buddy</p>
+            </div>
+        </nav>
+        </>
+    );
+}
+export default RiderEarnings;
