a7e6bf8f1e7379776515f58f875ed3f8a0398769
Decagon <decagon@Decagons-MacBook-Air-7.local>
2022-07-04 14:14:33 +0100

    rider profile code

---
+++ src/pages/RiderProfile/riderProfile.jsx
@@ -0,0 +1,11 @@
+import AuthNavbar from "../../components/AuthNavbar";
+
+
+const RiderProfile = () => {
+    return (
+        <>
+            <AuthNavbar />
+        </>
+    )
+}
+export default RiderProfile
