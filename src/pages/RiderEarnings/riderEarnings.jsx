4bbedfe64490a475a0293694c03e458905a544c9
Decagon <decagon@Decagons-MacBook-Air-7.local>
2022-07-07 07:04:39 +0100

    rider history refactor

--- src/pages/RiderEarnings/riderEarnings.jsx
+++ src/pages/RiderEarnings/riderEarnings.jsx
@@ -8,71 +8,71 @@ const RiderEarnings = () => {
     return (
         <>
         <AuthNavbar />
-        <div className="body-group">
             <div className="magenta-div"></div>
             {/* <br/>
             <br/> */}
-            <div className="wrapped-div">
-                <br/>
-                <div className="earnings-group">
-                    <div className="earnings-group-inner">
-                        <p className="earnings-right"><strong>Yesterday</strong>, 3:30PM</p>
-                        <div className="earnings-left">
-                        <input type="text" placeholder="Delivered" className="earnings-input"/>
-                        <p>N3,500.00</p>
-                        </div>   
-                    </div>
-                </div>
-                <br/>
-                <div className="earnings-group">
-                    <div className="earnings-group-inner">
-                        <p className="earnings-right"><strong>Yesterday</strong>, 3:30PM</p>
-                        <div className="earnings-left">
-                        <input type="text" placeholder="Delivered" className="earnings-input"/>
-                        <p>N3,500.00</p>
-                        </div>   
-                    </div>
-                </div>
-                <br/>
-                <div className="earnings-group">
-                    <div className="earnings-group-inner">
-                        <p className="earnings-right"><strong>Yesterday</strong>, 3:30PM</p>
-                        <div className="earnings-left">
-                        <input type="text" placeholder="Delivered" className="earnings-input" />
-                        <p>N3,500.00</p>
-                        </div>   
-                    </div>
-                </div>
-                <br/>
-                <div className="earnings-group">
-                    <div className="earnings-group-inner">
-                        <p className="earnings-right"><strong>Today</strong>, 10:15AM</p>
-                        <div className="earnings-left">
-                        <input type="text" placeholder="Delivered" className="earnings-input"/>
-                        <p>N2,200.00</p>
-                        </div>   
-                    </div>
-                </div>
-                <br/>
-                <div className="container1">
-                    <div className="column-text">
-                        <p>MY EARNINGS</p>
-                        <h1>N236,000</h1>
+            <div className="body-group">
+                <div className="wrapped-div">
+                    <br/>
+                    <div className="earnings-group">
+                        <div className="earnings-group-inner">
+                            <p className="earnings-right"><strong>Yesterday</strong>, 3:30PM</p>
+                            <div className="earnings-left">
+                            <input type="text" placeholder="Delivered" className="earnings-input"/>
+                            <p>N3,500.00</p>
+                            </div>   
+                        </div>
                     </div>
                     <br/>
-                    <div className="row-text">
-                        <div className="ride-div">
-                            <img src={smallcar} alt="" className="img-ride"/>
-                            <p>112 Rides</p>
+                    <div className="earnings-group">
+                        <div className="earnings-group-inner">
+                            <p className="earnings-right"><strong>Yesterday</strong>, 3:30PM</p>
+                            <div className="earnings-left">
+                            <input type="text" placeholder="Delivered" className="earnings-input"/>
+                            <p>N3,500.00</p>
+                            </div>   
                         </div>
-                        <div className="time-div">
-                            <img src={time} alt="" className="img-time"/>
-                            <p>890hrs . 20mins</p>
+                    </div>
+                    <br/>
+                    <div className="earnings-group">
+                        <div className="earnings-group-inner">
+                            <p className="earnings-right"><strong>Yesterday</strong>, 3:30PM</p>
+                            <div className="earnings-left">
+                            <input type="text" placeholder="Delivered" className="earnings-input" />
+                            <p>N3,500.00</p>
+                            </div>   
                         </div>
-                    </div> 
+                    </div>
+                    <br/>
+                    <div className="earnings-group">
+                        <div className="earnings-group-inner">
+                            <p className="earnings-right"><strong>Today</strong>, 10:15AM</p>
+                            <div className="earnings-left">
+                            <input type="text" placeholder="Delivered" className="earnings-input"/>
+                            <p>N2,200.00</p>
+                            </div>   
+                        </div>
+                    </div>
+                    <br/>
+                    <div className="container1">
+                        <div className="column-text">
+                            <p>MY EARNINGS</p>
+                            <h1>N236,000</h1>
+                        </div>
+                        <br/>
+                        <div className="row-text">
+                            <div className="ride-div">
+                                <img src={smallcar} alt="" className="img-ride"/>
+                                <p>112 Rides</p>
+                            </div>
+                            <div className="time-div">
+                                <img src={time} alt="" className="img-time"/>
+                                <p>890hrs . 20mins</p>
+                            </div>
+                        </div> 
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
--- src/pages/RiderHistory/riderHistory.jsx
+++ src/pages/RiderHistory/riderHistory.jsx
@@ -6,55 +6,56 @@ const RiderHistory = () => {
     return (
     <>
         <AuthNavbar />
-
-        <div className="history-container">
-            <h1>Ride History</h1>
-            <div className="input-right">
-                <input type="text" placeholder="Filter" className="filter-input" />
-                <img src={filter} alt="" className="filter-img" />
-            </div>  
-        </div>
-        <hr className="first-line"/>
-            <div className="description">
-                <h3>Location</h3>
-                <h3>Receiver's Contact</h3>
-                <h3>Date/Time</h3>
-                <h3>Status</h3>
+        <div className="whole-body">
+            <div className="history-container">
+                <h1>Ride History</h1>
+                <div className="input-right">
+                    <input type="text" placeholder="Filter" className="filter-input" />
+                    <img src={filter} alt="" className="filter-img" />
+                </div>  
             </div>
-        <div className="container2">
-            <div className="row1">
-                <p className="location-parag">Lagos</p>
-                <p>07044234422</p>
-                <p>June,Mon 5, 2022  4:15 PM</p>
-                <p className="status-pending">Pending</p>
-            </div>
-            <hr/>
-            <div className="row2">
-                <p className="location-parag">Abuja</p>
-                <p>08099775244</p>
-                <p>June,Thurs 5, 2022  1:00 PM</p>
-                <p className="status-delivered">Delivered</p>
-            </div>
-            <hr/>
-            <div className="row3">
-                <p className="location-parag">Lagos</p>
-                <p>07044234422</p>
-                <p>May,Tues 5, 2022  4:15 PM</p>
-                <p className="status-delivered">Pending</p>
-            </div>
-            <hr/>
-            <div className="row4">
-                <p className="location-parag">Abuja</p>
-                <p>08099775244</p>
-                <p>May,Fri 5, 2022  1:00 PM</p>
-                <p className="status-delivered">Delivered</p>
-            </div>
-            <hr/>
-            <div className="row5">
-                <p className="location-parag">Abuja</p>
-                <p>08099775244</p>
-                <p>April,Mon 5, 2022  1:00 PM</p>
-                <p className="status-delivered">Delivered</p>
+            <hr className="first-line"/>
+                <div className="description">
+                    <h3 className="desc-location">Location</h3>
+                    <h3 className="desc-contact">Receiver's Contact</h3>
+                    <h3 className="desc-date">Date/Time</h3>
+                    <h3 className="desc-status">Status</h3>
+                </div>
+            <div className="container2">
+                <div className="row1">
+                    <p className="location-parag">Lagos</p>
+                    <p className="contact">07044234422</p>
+                    <p className="date-time">June,Mon 5, 2022  4:15 PM</p>
+                    <p className="status-pending">Pending</p>
+                </div>
+                <hr className="demacated-line"/>
+                <div className="row2">
+                    <p className="location-parag">Abuja</p>
+                    <p className="contact">08099775244</p>
+                    <p className="date-time">June,Thurs 5, 2022  1:00 PM</p>
+                    <p className="status-delivered">Delivered</p>
+                </div>
+                <hr className="demacated-line"/>
+                <div className="row3">
+                    <p className="location-parag">Lagos</p>
+                    <p className="contact">07044234422</p>
+                    <p className="date-time">May,Tues 5, 2022  4:15 PM</p>
+                    <p className="status-delivered">Pending</p>
+                </div>
+                <hr className="demacated-line"/>
+                <div className="row4">
+                    <p className="location-parag">Abuja</p>
+                    <p className="contact">08099775244</p>
+                    <p className="date-time">May,Fri 5, 2022  1:00 PM</p>
+                    <p className="status-delivered">Delivered</p>
+                </div>
+                <hr className="demacated-line"/>
+                <div className="row5">
+                    <p className="location-parag">Abuja</p>
+                    <p className="contact">08099775244</p>
+                    <p className="date-time">April,Mon 5, 2022  1:00 PM</p>
+                    <p className="status-delivered">Delivered</p>
+                </div>
             </div>
         </div>
     </>
